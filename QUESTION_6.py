23156e2c8decc6fe39b07808a4d86cd1ff7e8df7
yadavshivjeet <161686742+yadavshivjeet@users.noreply.github.com>
2024-04-08 14:34:10 +0530

    Add files via upload

---
+++ QUESTION_6.py
@@ -0,0 +1,17 @@
+#6 Perform bitwise operations on any two integers.
+a=5
+b=3
+#and
+print(a&b)
+#or
+print(a|b)
+#not
+print(~a)
+#xor
+print(a^b)
+#left shift
+b=5
+print(b<<2)
+#right shift
+c=16
+print(c>>2)
